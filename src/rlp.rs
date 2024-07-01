a39826ead3b67a566ee38e3b50b13ed20d6c95a6
hanhvn <vonhathanh@gmail.com>
2024-07-01 16:57:18 +0700

    implement rlp serialization

--- src/rlp.rs
+++ src/rlp.rs
@@ -1,57 +1,86 @@
-use std::fmt::Debug;
+pub enum RLPInput<T> {
+    Bytes(Vec<u8>),
+    List(Vec<T>)
+}
 
-#[derive(Debug)]
-pub struct Bytes(Vec<u8>);
+// if x is a single byte and x < 128, return x
+// else if x.len < 56, return (128+||x||).concat(x)
+// else return (183 + ||BE(||x||)||).concat(BE(||x||)).concat(x)
+// else return empty 
+fn rb(x: Vec<u8>) -> Vec<u8> {
+    println!("RB called");
 
-impl Bytes {
-    pub fn new(x: Vec<u8>) -> Self {
-        Bytes(x)
+    let mut result: Vec<u8> = vec![];
+
+    if x.len() == 1 && x[0] < 128 {
+        result.push(x[0]);
+
+    } else if x.len() < 56 {
+        result.push(128 + x.len() as u8);
+        result.extend(x);
+
+    } else if x.len() < usize::MAX {
+        let bex = x.len().to_be_bytes().to_vec();
+
+        result.push(183 + bex.len() as u8);
+        result.extend(bex);
+        result.extend(x);
+    }
+
+    result
+}
+
+fn rl<T: 'static>(x: Vec<T>) -> Vec<u8> {
+    println!("RL called");
+
+    let mut result: Vec<u8> = vec![];
+
+    let sx = serialize(x);
+    // If the concatenated serialisations (sx) of each contained item is less than 56 bytes in length, 
+    // return (192 + ||sx||).concat(sx)
+    if sx.len() < 56 && sx.len() > 0 {
+        result.push(192 + sx.len() as u8);
+        result.extend(sx);
+    } 
+    // Otherwise, if sx < 2^64 and sx != empty list
+    // return (247 + ||BE(||sx||)||).concat(BE(||sx||)).concat(sx)
+    else if sx.len() < u64::MAX.try_into().unwrap() {
+        let besx = sx.len().to_be_bytes().to_vec();
+
+        result.push(247 + besx.len() as u8);
+        result.extend(besx);
+        result.extend(sx);
+    }
+
+    result
+}
+
+// run rlp() for every item in x
+fn serialize<T: 'static>(x: Vec<T>) -> Vec<u8> {
+    let mut result: Vec<u8> = vec![];
+    for item in x {
+        result.extend(rlp(RLPInput::<u8>::Bytes(serialize_item(item))))
+    }
+    result
+}
+
+fn serialize_item<T: 'static>(item: T) -> Vec<u8> {
+    match item {
+        item if std::any::TypeId::of::<T>() == std::any::TypeId::of::<u8>() => {
+            vec![unsafe { std::mem::transmute_copy(&item) }]
+        },
+        item if std::any::TypeId::of::<T>() == std::any::TypeId::of::<Vec<u8>>() => {
+            unsafe { std::mem::transmute_copy::<T, Vec<u8>>(&item) }
+        },
+        _ => rlp(RLPInput::List(item)),
     }
 }
-
-#[derive(Debug)]
-pub struct List<T>(Vec<T>);
-
-impl<T> List<T> {
-    pub fn new(x: Vec<T>) -> Self {
-        List(x)
-    }
-}
-
-// Define an enum to distinguish between the types
-#[derive(Debug)]
-pub enum RLPInputType {
-    BytesArray,
-    ListOfItems
-}
-
-// define a trait that both types of input will implement
-pub trait RLPInput: Debug {
-    fn get_type(&self) -> RLPInputType;
-}
-
-// implement it for bytes array first
-impl RLPInput for Bytes {
-    fn get_type(&self) -> RLPInputType {
-        RLPInputType::BytesArray
-    }
-}
-
-// implement the trait for a list of arbitrary values
-impl<T: Debug> RLPInput for List<T> {
-    fn get_type(&self) -> RLPInputType {
-        RLPInputType::ListOfItems
-    }
-} 
 
 // rlp(x) = rb(x) if x is bytes
 // else rl(x)
-
-
-pub fn rlp<T: RLPInput>(x: T)-> Vec<u8> {
-    match x.get_type() {
-        RLPInputType::BytesArray => println!("Bytes array type, calling RB()"),
-        RLPInputType::ListOfItems => println!("List of arbitrary values, call RL()"),
+pub fn rlp<T: 'static>(x: RLPInput<T>)-> Vec<u8> {
+    match x {
+        RLPInput::Bytes(bytes) => rb(bytes),
+        RLPInput::List(list) => rl(list),
     }
-    vec![]
 }
--- tests/rlp.rs
+++ tests/rlp.rs
@@ -2,8 +2,7 @@ use rust_tutorial::rlp::*;
 
 #[test]
 fn polynomial() {
-    let bytes_input = Bytes::new(vec![1, 2, 3, 4]);
-    rlp(bytes_input);
-    let string_input = List::new(vec!["aa".to_string(), "bb".to_string(), "cc".to_string()]);
-    rlp(string_input);
+    // let rb_result = rlp(RLPInput::<u8>::Bytes(vec![1, 2, 3, 4]));
+    // println!("rb: {:?}", rb_result);
+    rlp(RLPInput::List(vec!["aa".to_string(), "bb".to_string(), "cc".to_string()]));
 }
