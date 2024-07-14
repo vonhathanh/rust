bc985a3af9b52c1becc3d84ee7ed11a9b7953e49
hanhvn <vonhathanh@gmail.com>
2024-07-14 13:44:10 +0700

    use correct type for all structs

--- src/evm.rs
+++ src/evm.rs
@@ -1,17 +1,21 @@
-use crate::state::WorldState;
+use std::collections::HashMap;
+
+use alloy_primitives::{B256, U256};
+
+use crate::state::{SubState, WorldState};
 
 pub struct EVM {
     // Global system state
-    worldState: WorldState,
+    world_state: WorldState,
     // Sub state
-    subState: SubState,
+    sub_state: SubState,
     // A simple stack with word size = 256 bit. Maximum size of the stack is 1024
-    stack: Stack,
+    stack: Vec<B256>,
     // A simple word-addressed byte array (address size = 2 bytes, word size = 32 bytes)
-    memory: Memory,
+    memory: Vec<B256>,
     // Unlike memory, which is volatile, storage is non volatile and is maintained as part of the system state
-    storage: Storage,
+    storage: HashMap<B256, B256>,
     // Remaining gas for the execution
-    gas: Gas,
-    executionContext: Context,
+    gas: U256,
+    // execution_context: Context,
 }
