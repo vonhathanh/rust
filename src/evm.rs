7aef41eaca296620950889adf288d15140baee6f
hanhvn <vonhathanh@gmail.com>
2024-07-17 06:37:49 +0700

    make all field in struct public

--- src/evm.rs
+++ src/evm.rs
@@ -1,52 +1,52 @@
-use std::{collections::HashMap, marker::PhantomData};
+use std::borrow::BorrowMut;
 
-use alloy_primitives::{Address, B256, U256};
+use alloy_primitives::{Address, U256};
 use bytes::Bytes;
 
-use crate::{block::BlockHeader, state::{SubState, WorldState}};
+use crate::{block::BlockHeader, state::{self, EVMState, SubState, WorldState}};
 
-pub struct EVM {
+pub struct EVM<'a> {
     // Global system state (sigma)
-    world_state: WorldState,
+    pub world_state: &'a mut WorldState,
     // Sub state (A)
-    sub_state: SubState,
-    // A simple stack with word size = 256 bit. Maximum size of the stack is 1024
-    stack: Vec<B256>,
-    // A simple word-addressed byte array (address size = 2 bytes, word size = 32 bytes)
-    memory: HashMap<u16, B256>,
-    // Unlike memory, which is volatile, storage is non volatile and is maintained as part of the system state
-    storage: HashMap<B256, B256>,
-    // Remaining gas for the execution
-    gas: U256,
+    pub sub_state: &'a mut SubState,
     // there are several pieces of important information used in the execution environment that the
     // execution agent must provide (I)
-    execution_env: ExecutionEnvironment,
+    pub execution_env: &'a ExecutionEnvironment,
+    // Machine state (µ: mu)
+    pub state: &'a mut EVMState,
+}
+
+impl<'a> EVM<'a> {
+    pub fn execute(&mut self) {
+        while self.state.pc < self.ex
+    }
 }
 
 pub struct ExecutionEnvironment {
     // Ia, the address of the account which owns the code that is executing.
-    contract_account: Address,
+    pub contract_account: Address,
     // Io, the sender address of the transaction that originated this execution.
-    tx_origin: Address,
+    pub tx_origin: Address,
     // Ip, the price of gas paid by the signer of the transaction that originated this execution. This is defined
     // as the effective gas price p
-    gas_price: U256,
+    pub gas_price: U256,
     // Id, the byte array that is the input data to this execution; if the execution agent is a transaction,
     // this would be the transaction data.
-    input_data: Bytes,
+    pub input_data: Bytes,
     // Is, the address of the account which caused the code to be executing; if the execution agent is a
     // transaction, this would be the transaction sender.
-    sender: Address,
+    pub sender: Address,
     // Iv, the value, in Wei, passed to this account as part of the same procedure as execution; if the
     // execution agent is a transaction, this would be the transaction value.
-    value: U256,
+    pub value: U256,
     // Ib, the byte array that is the machine code to be executed
-    byte_code: Bytes,
+    pub byte_code: Bytes,
     // IH, the block header of the present block. 
-    block_header: BlockHeader,
+    pub block_header: BlockHeader,
     // Ie, the depth of the present message-call or contract-creation (i.e. the number of CALLs or
     // CREATE(2)s being executed at present).
-    depth: u64,
+    pub depth: u64,
     // Iw, the permission to make modifications to the state
-    permission: bool,
+    pub permission: bool,
 }
--- src/state.rs
+++ src/state.rs
@@ -14,43 +14,43 @@ pub struct WorldState {
     // Secondly, it allows any previous state (whose root hash is known) to be recalled by simply
     // altering the root hash accordingly. Since we store all such root hashes in the blockchain, we are able to
     // trivially revert to old states.
-    trie: HashMap<Address,  AccountState>,
+    pub trie: HashMap<Address,  AccountState>,
 }
 
 pub struct AccountState {
     // Number of transactions sent from this address or 
     // in the case of contract-account, the number of contract-creation made by this account
-    nonce: U256,
+    pub nonce: U256,
     // Number of Wei owned by this address
-    balance: U256,
+    pub balance: U256,
     // A 256 bit hash of the root node of the trie that encode the storage content of an account
     // (a mapping between 256 bit integer values)
     // encoded into the trie as: keccak256(all keys) -> RLP encoded of all values
     // Note: storage_root is not a "physical" member of the AccountState and does not contribute to it serialization
-    storage_root: B256,
+    pub storage_root: B256,
     // The hash of the EVM byte codes of this address
-    code_hash: B256,
+    pub code_hash: B256,
     // code: if None, do nothing otherwise load the code from storage if it need to execute calls
-    code: Option<Bytes>
+    pub code: Option<Bytes>
 }
 
 pub struct SubState {
     // Accrued information that is acted upon immediately following the transaction
     // A_s: set of accounts that will be discarded following the transaction completion
-    self_destruct_set: HashSet<Address>,
+    pub self_destruct_set: HashSet<Address>,
     // A_l: the log series: series of archived and indexable 'checkpoints' in VM code execution
     // that allow contract-calls to be easily tracked by onlooker external to the Ethereum world
-    logs: Vec<Bytes>,
+    pub logs: Vec<Bytes>,
     // A_t: set of touched accounts, the empty ones are deleted after transaction end
-    touched_accounts: HashSet<Address>,
+    pub touched_accounts: HashSet<Address>,
     // A_r: refund balance, increased through using the SSTORE instruction to reset contract storage
-    refund: B256,
+    pub refund: B256,
     // EIP-2929, A_a: set of accessed account addresses, A_k, set of accessed storage keys
     // A_k = (address, 32-byte storage slot) why 32-byte? Because 32-byte = 256 bit
     // Note: In the Yellow Paper, A_a is initialized as Pi, set of precompiled addresses, we'll ignore them for now
     // since we don't even know what the heck they are
-    accessed_accounts: HashSet<Address>,
-    accessed_storage: HashMap<Address, Bytes>,
+    pub accessed_accounts: HashSet<Address>,
+    pub accessed_storage: HashMap<Address, Bytes>,
 }
 
 // Machine state (µ)
@@ -62,17 +62,17 @@ pub struct SubState {
 // empty set -> continue, empty sequence -> halt
 pub struct EVMState {
     // gas available
-    gas: u64,
+    pub gas: u64,
     // program counter
-    pc: U256,
+    pub pc: U256,
     // memory contents: a series of zeroes of size 2^256
     // it's a series, so we use Vec, but no computer can allocate Vec size of 2^256 (no program ever need that fucking memory)
     // so I think we'll be fine
-    m: Vec<B256>,
+    pub m: Vec<B256>,
     // active number of words in memory
-    i: usize,
+    pub i: usize,
     // stack contents
-    s: Vec<B256>,
+    pub s: Vec<B256>,
     // returndata buffer
-    o: Bytes
+    pub o: Bytes
 }
