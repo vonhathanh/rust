b32e60301f6db09be04d23581455312ef660f735
hanhvn <vonhathanh@gmail.com>
2024-07-18 19:13:55 +0700

    try to init all neccessary variables

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
+use alloy_primitives::{hex::FromHex, Address, B256, U256};
+use state::{EVMState, SubState, WorldState};
+use transaction::Transaction;
+
+mod block;
+mod evm;
+mod functions;
+mod operations;
 mod state;
 mod transaction;
-mod evm;
-mod block;
-mod operations;
-mod functions;
 
-fn main() {}
+fn main() {
+    let mut world_state = WorldState::new();
+    let mut evm_state = EVMState::new();
+    let mut accrued_substate = SubState::new();
+    let tx = Transaction::new(
+        0,
+        U256::from(1),
+        100000,
+        Some(Address::from_hex("0x6148ce093dcbd629cfbc4203c18210567d186c66").unwrap()),
+        U256::ZERO,
+        B256::ZERO,
+        B256::ZERO,
+        None,
+        Some(1),
+        Some(1),
+        None,
+        None,
+        None,
+        Some(U256::from(1000)),
+        None,
+        None
+    );
+}
