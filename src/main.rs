4770ca1341ac38a4dbb293be826b6095739b7f69
hanhvn <vonhathanh@gmail.com>
2024-07-21 06:56:47 +0700

    init all types in main()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use alloy_primitives::{hex::FromHex, Address, B256, U256};
+use block::BlockHeader;
+use evm::{ExecutionEnvironment, EVM};
 use state::{EVMState, SubState, WorldState};
 use transaction::Transaction;
 
@@ -13,6 +15,8 @@ fn main() {
     let mut world_state = WorldState::new();
     let mut evm_state = EVMState::new();
     let mut accrued_substate = SubState::new();
+    let mut block_header = BlockHeader::new();
+    let default_sender = Address::from_hex("0xA160cdAB225685dA1d56aa342Ad8841c3b53f291").unwrap();
     let tx = Transaction::new(
         0,
         U256::from(1),
@@ -29,6 +33,22 @@ fn main() {
         None,
         Some(U256::from(1000)),
         None,
-        None
+        None,
     );
+
+    let execute_env = ExecutionEnvironment::new(
+        tx.to.unwrap(),
+        default_sender,
+        tx.gas_price.unwrap(),
+        tx.data.unwrap(),
+        default_sender,
+        tx.value,
+        world_state.trie.get(&tx.to.unwrap()).unwrap().code.unwrap(),
+        &block_header,
+        1,
+        true,
+    );
+
+    let mut evm = EVM::new(&mut world_state, &mut accrued_substate, &execute_env);
+    evm.execute_tx(&tx);
 }
